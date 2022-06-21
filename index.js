da9e001781e5b4741aeac8cfe59afe836afdbf54
Brian Allen <vecter1180@gmail.com>
2022-06-21 19:03:08 -0400

    commit tree a26306168060ff877e5524b37fa499c523c568fa

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+for(let age = 30; age < 40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+    
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        ;
+    }
+    
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+const names = ["Guadalup", "Ollie", "Aki"]
+let celly = 'birthday'
+//let i = 0
+
+
+function writeCards(celly) {
+    
+    for (let i = 0; i < names.length; i++){
+        console.log(`Thanks you, ${names[i]}, for the wonderful surprise gift ${celly}!`)
+        
+    return newArray
+    }
+}
+
+
